9fb29368825311895d1f0881bb9e1622a566d758
davidnene <davienesh4@gmail.com>
2022-09-28 14:41:20 +0300

    commit tree 87c281ec9a5937ffc037ff0e01a342c62a98df2d

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,9 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def student_details
+    student = Student.find(params[:id])
+    render json: student
+  end
+
 end
